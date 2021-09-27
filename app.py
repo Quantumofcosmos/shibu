064e23472ffa850a395ea168870c74969cf66dba
nidhinpaul90 <34165889+nidhinpaul90@users.noreply.github.com>
2021-09-27 16:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g, session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -12,27 +12,27 @@ recent = ["", "", "", "", ""]
 #lol
 @app.route("/", methods=["GET", "POST"])
 def index():
-    status=0
-    realflag=False
+    session['status'] = 0
+    session['realflag'] = False
     if request.method == "POST":
-        msg= request.form.get("name")
+        session['msg']= request.form.get("name")
         if msg is None:
-            status=2
-            return render_template("index.html", status=status)
-        count=0
-        while count < 5:
-            flag = recent[count] in msg
+            session['status']=2
+            return render_template("index.html", status=session['status'])
+        session['count']=0
+        while session['count'] < 5:
+            flag = recent[session['count']] in msg
             if flag:
-                realflag=True
-            count += 1
-        if realflag:
-            status=2
-            return render_template("index.html", status=status)
-        recent.extend(msg)
+                session['realflag']=True
+            session['count'] += 1
+        if session['realflag']:
+            session['status']=2
+            return render_template("index.html", status=session['status'])
+        recent.extend(session['msg'])
         recent.pop(0)
         db.execute("INSERT INTO operation (msg) VALUES (:msg)",
-                {"msg": msg})
+                {"msg": session['msg']})
         db.commit()
-        status=1
+        session['status']=1
 
-    return render_template("index.html", status=status)
+    return render_template("index.html", status=session['status'])
