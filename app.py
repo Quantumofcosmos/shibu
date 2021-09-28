e50bbb60863ec2d4cf586122e07631e3863a1573
nidhinpaul90 <34165889+nidhinpaul90@users.noreply.github.com>
2021-09-28 22:00:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,7 @@ def index():
         if "https://" in msg:
             status=2
             return render_template("index.html", status=status)
-        if "@" in msg:
-            status=2
-            return render_template("index.html", status=status)
+      
             
             
         
